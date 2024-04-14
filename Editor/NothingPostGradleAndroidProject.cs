5d0d649b2144927f94fdb1c6cd65da37c33bad9d
p.shestakov <p.shestakov@gaijin.team>
2024-04-14 11:59:26 +0300

    added ability to patch AndroidManifest.xml with proper 'NothingKey' (also known as Api Key) while build process

---
+++ Editor/NothingPostGradleAndroidProject.cs
@@ -0,0 +1,49 @@
+﻿#if UNITY_EDITOR
+#if UNITY_ANDROID
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Android;
+using UnityEngine;
+
+namespace NothingOS
+{
+    public class NothingPostGradleAndroidProject : IPostGenerateGradleAndroidProject
+    {
+        private const string ANDROID_MANIFEST = "AndroidManifest.xml";
+        public int callbackOrder => 0;
+
+        public void OnPostGenerateGradleAndroidProject(string path)
+        {
+            var androidManifestPath = Path.Combine(path, "src", "main", ANDROID_MANIFEST);
+            var androidManifestFile = new FileInfo(androidManifestPath);
+            if (!androidManifestFile.Exists)
+            {
+                Debug.LogError($"OnPostGenerateGradleAndroidProject: {ANDROID_MANIFEST} not found at path {androidManifestPath}");
+                return;
+            }
+
+            Debug.Log($"OnPostGenerateGradleAndroidProject: modifying {ANDROID_MANIFEST} at path {androidManifestPath}");
+
+            var settings = NothingSettings.FindInAssets();
+            if (settings == null)
+            {
+                Debug.LogError($"OnPostGenerateGradleAndroidProject: {typeof(NothingSettings)} not found in Assets");
+                return;
+            }
+
+            var settingsPath = AssetDatabase.GetAssetPath(settings);
+            Debug.Log($"OnPostGenerateGradleAndroidProject: {typeof(NothingSettings)} found at path {settingsPath}");
+
+            var applicationTag = "</application>";
+            var dataToInject = $"<meta-data android:name=\"NothingKey\" android:value=\"{settings.apiKey}\" />";
+
+            var xml = File.ReadAllText(androidManifestFile.FullName);
+            xml = xml.Replace(applicationTag, $"  {dataToInject}{Environment.NewLine}  {applicationTag}");
+            File.WriteAllText(androidManifestFile.FullName, xml);
+            Debug.Log($"OnPostGenerateGradleAndroidProject: {ANDROID_MANIFEST} patched successfully");
+        }
+    }
+}
+#endif
+#endif
---
+++ Runtime/NothingSettings.cs
@@ -0,0 +1,33 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace NothingOS
+{
+    [CreateAssetMenu(fileName = "NothingSettings", menuName = "Nothing GDK/Create Settings")]
+    public class NothingSettings : ScriptableObject
+    {
+        [SerializeField]
+        private string _apiKey;
+        public string apiKey => _apiKey;
+
+#if UNITY_EDITOR
+        public static NothingSettings FindInAssets()
+        {
+            var guids = AssetDatabase.FindAssets("t:Object");
+            foreach (var guid in guids)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                var obj = AssetDatabase.LoadAssetAtPath<NothingSettings>(path);
+                if (obj == null)
+                    continue;
+
+                return obj;
+            }
+
+            return null;
+        }
+#endif
+    }
+}
